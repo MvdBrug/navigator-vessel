d02780c5d372d6e85cd750bd7cebd136696bb4b0
MvdBrug <Meintebrug@hotmail.com>
2026-03-16 16:50:45 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    updated files so users get the latest version.
    ───────────────────────────────────────────────────────────── */
 
-const CACHE_NAME = "nav-gas-ess-v9";
+const CACHE_NAME = "nav-gas-ess-v10";
 
 const urlsToCache = [
   "/",
